0d904ca949c8211b5b07742131fb8c52238d55ee
Hayden030127 <teomeehua@gmail.com>
2024-06-11 03:02:25 +0800

    enhance ui

--- src/modals/AddEditTaskModal.jsx
+++ src/modals/AddEditTaskModal.jsx
@@ -90,8 +90,8 @@ function AddEditTaskModal({type , device, setOpenAddEditTask, taskIndex, pervCol
         setOpenAddEditTask(false)
     }}
     className={
-        device === 'mobile' ? ' py-6 px-6 pb-50 absolute overflow-y-scroll left-0 flex right-0 bottom-[-100vh] top-0 bg-[#00000080]' 
-        : 'py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-0 top-0 bg-[#00000080]'
+        device === 'mobile' ? ' scrollbar-hide py-6 px-6 pb-50 absolute overflow-y-scroll left-0 flex right-0 bottom-[-100vh] top-0 bg-[#00000080]' 
+        : 'scrollbar-hide py-6 px-6 pb-40 absolute overflow-y-scroll left-0 flex right-0 bottom-0 top-0 bg-[#00000080]'
     }
     >
         {/* Modal Section */}
